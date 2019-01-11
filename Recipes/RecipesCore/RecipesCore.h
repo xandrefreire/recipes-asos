41f5a1bdcda1a6b11f26a895fe55ab4a276ed188
Alexandre Freire <alexandre@mbpdealexandre.home>
2019-01-11 09:23:25 +0000

    Add RecipesCore framework

---
+++ Recipes/RecipesCore/RecipesCore.h
@@ -0,0 +1,19 @@
+//
+//  RecipesCore.h
+//  RecipesCore
+//
+//  Created by Alexandre Freire on 11/01/2019.
+//  Copyright © 2019 Alexandre Freire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RecipesCore.
+FOUNDATION_EXPORT double RecipesCoreVersionNumber;
+
+//! Project version string for RecipesCore.
+FOUNDATION_EXPORT const unsigned char RecipesCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecipesCore/PublicHeader.h>
+
+
