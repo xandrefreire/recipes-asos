bee76424b7d5b0fee505123456a30764e8a93e3d
Alexandre Freire <xandrefreire@gmail.com>
2019-01-12 12:09:03 +0000

    Add Rx-load version to WebService

--- Recipes/RecipesCore/WebService/WebService.swift
+++ Recipes/RecipesCore/WebService/WebService.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import RxSwift
 
 public enum WebServiceError: Error {
     case badStatus(Int, Data)
@@ -42,5 +43,55 @@ final class WebService {
             }
         }
         task.resume()
+    }
+    
+    func load<T>(_ type: T.Type, from endpoint: Endpoint) -> Observable<T> where T: Decodable {
+        let decoder = self.decoder
+        let request = endpoint.request(with: baseURL)
+        
+        return session.rx.data(request: request)
+            .map { try decoder.decode(T.self, from: $0) }
+            .catchError { error in
+                guard let webServiceError = error as? WebServiceError else {
+                    throw error
+                }
+                
+                guard case let .badStatus(code, data) = webServiceError else {
+                    throw error
+                }
+                
+                throw WebServiceError.badStatus(code, data)
+        }
+    }
+}
+
+private extension Reactive where Base: URLSession {
+    func data(request: URLRequest) -> Observable<Data> {
+        return Observable.create { observer in
+            let task = self.base.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    observer.onError(error)
+                } else {
+                    guard let httpResponse = response as? HTTPURLResponse else {
+                        fatalError("Unsupported protocol")
+                    }
+                    
+                    if 200 ..< 300 ~= httpResponse.statusCode {
+                        if let data = data {
+                            observer.onNext(data)
+                        }
+                        observer.onCompleted()
+                    } else {
+                        observer.onError(WebServiceError.badStatus(httpResponse.statusCode, data ?? Data()))
+                    }
+                }
+            }
+            
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
     }
 }
