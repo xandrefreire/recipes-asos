da4d8febc4a22a35b52ff0acaa8a7c109fd9f59d
Alexandre Freire <xandrefreire@gmail.com>
2019-01-14 13:40:21 +0000

    Add error handling

---
+++ Recipes/RecipesCore/Common/Views/ErrorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ErrorViewController.swift
+//  RecipesCore
+//
+//  Created by Alexandre Freire on 14/01/2019.
+//  Copyright © 2019 Alexandre Freire. All rights reserved.
+//
+
+import UIKit
+
+class ErrorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
